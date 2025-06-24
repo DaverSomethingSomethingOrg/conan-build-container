1b241410048d564c05d2384909fc6fddaf575d8e
David L. Armstrong <1403560+daversomethingsomething@users.noreply.github.com>
2025-06-24 10:56:15 -0700

    - Complete revamp for multiplatform images - Removed QEMU content - Switched to Python virtualenv
    Signed-off-by: David L. Armstrong <1403560+daversomethingsomething@users.noreply.github.com>

--- README.md
+++ README.md
@@ -9,21 +9,6 @@ Reference:
 
 ## License and Copyright
 
-### Source files included in this repo
-
 Copyright © 2025 David L. Armstrong
 
 [Apache-2.0](LICENSE.txt)
-
-### QEMU content
-
-QEMU is a trademark of Fabrice Bellard.
-
-The QEMU binaries included here are copyrighted and licensed by the QEMU team.
-For more information on the QEMU License terms:
-[view them here](https://gitlab.com/qemu-project/qemu/-/blob/master/LICENSE?ref_type=heads)
-
-Some parts are licensed according to the
-[GNU Lesser General Public License](COPYING.LIB) and the
-[GNU General Public License](COPYING) with the respective
-licenses included here.  They can also be [viewed here](http://www.gnu.org/licenses/).
---
+++ build_containers.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/bash
+
+machine_arch=$(uname -m)
+case "${machine_arch}" in
+    aarch64|arm64)
+        docker_platform="linux/arm64"
+        ;;
+    x86_64)
+        docker_platform="linux/amd64"
+        ;;
+    *)
+        echo "ERROR: unsupported architecture: '${machine_arch}'"
+        exit 1
+        ;;
+esac
+
+PKG_PREFIX="opt-toolchain-"
+TOOLCHAIN_PREFIX="/opt/toolchain"
+
+docker build \
+         --no-cache \
+         --file src/Dockerfile-almalinux \
+         --target conan-base \
+         --platform ${docker_platform} \
+         --tag ghcr.io/daversomethingsomethingorg/conan-base-almalinux:latest \
+         src \
+&& docker push \
+            --platform ${docker_platform} \
+            ghcr.io/daversomethingsomethingorg/conan-base-almalinux:latest
+&& docker build \
+            --file src/Dockerfile-almalinux \
+            --target conan-bootstrap \
+            --platform ${docker_platform} \
+            --tag ghcr.io/daversomethingsomethingorg/conan-bootstrap-almalinux:latest \
+            src \
+&& docker push \
+            --platform ${docker_platform} \
+            ghcr.io/daversomethingsomethingorg/conan-bootstrap-almalinux:latest \
+&& docker build \
+            --file src/Dockerfile-almalinux \
+            --target conan-build \
+            --build-arg PKG_PREFIX="${PKG_PREFIX}" \
+            --build-arg TOOLCHAIN_PREFIX="${TOOLCHAIN_PREFIX}" \
+            --platform ${docker_platform} \
+            --tag ghcr.io/daversomethingsomethingorg/conan-build-almalinux:latest \
+            src \
+&& docker push \
+            --platform ${docker_platform} \
+            ghcr.io/daversomethingsomethingorg/conan-build-almalinux:latest
+
+docker build \
+            --no-cache \
+            --file src/Dockerfile-ubuntu \
+            --target conan-base \
+            --platform ${docker_platform} \
+            --tag ghcr.io/daversomethingsomethingorg/conan-base-ubuntu:latest \
+            src \
+&& docker push \
+            --platform ${docker_platform} \
+            ghcr.io/daversomethingsomethingorg/conan-base-ubuntu:latest
+&& docker build \
+            --file src/Dockerfile-ubuntu \
+            --target conan-bootstrap \
+            --platform ${docker_platform} \
+            --tag ghcr.io/daversomethingsomethingorg/conan-bootstrap-ubuntu:latest \
+            src \
+&& docker push \
+            --platform ${docker_platform} \
+            ghcr.io/daversomethingsomethingorg/conan-bootstrap-ubuntu:latest \
+&& docker build \
+            --file src/Dockerfile-ubuntu \
+            --target conan-build \
+            --build-arg PKG_PREFIX="${PKG_PREFIX}" \
+            --build-arg TOOLCHAIN_PREFIX="${TOOLCHAIN_PREFIX}" \
+            --platform ${docker_platform} \
+            --tag ghcr.io/daversomethingsomethingorg/conan-build-ubuntu:latest \
+            src \
+&& docker push \
+            --platform ${docker_platform} \
+            ghcr.io/daversomethingsomethingorg/conan-build-ubuntu:latest
