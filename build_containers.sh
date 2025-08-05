39282616c85704e9a176142bf070b9f332f7379d
David L. Armstrong <1403560+daversomethingsomething@users.noreply.github.com>
2025-08-05 16:07:53 -0700

    switch to arch-specific tags
    Signed-off-by: David L. Armstrong <1403560+daversomethingsomething@users.noreply.github.com>

--- build_containers.sh
+++ build_containers.sh
@@ -5,6 +5,7 @@ machine_arch=$(uname -m)
 case "${machine_arch}" in
     aarch64|arm64)
         docker_platform="linux/arm64"
+        machine_arch=aarch64
         ;;
     x86_64)
         docker_platform="linux/amd64"
@@ -19,46 +20,46 @@ cp src/requirements.txt src/almalinux
 mkdir -p src/almalinux/config/etc/pki/ca-trust/source/anchors
 cp src/DaverSomethingSomethingRootCA.crt src/almalinux/config/etc/pki/ca-trust/source/anchors
 
-PKG_PREFIX="opt-toolchain-"
 TOOLCHAIN_PREFIX="/opt/toolchain"
+PKG_PREFIX="opt-toolchain-"
 
+#         --no-cache \
 docker build \
-         --no-cache \
          --target conan-base \
          --platform ${docker_platform} \
-         --tag nexus.homelab/conan-base-almalinux:latest \
+         --tag "nexus.homelab/conan-base-almalinux:${machine_arch}-latest" \
          src/almalinux \
 && docker push \
             --platform ${docker_platform} \
-            nexus.homelab/conan-base-almalinux:latest \
+            "nexus.homelab/conan-base-almalinux:${machine_arch}-latest" \
 && docker build \
             --target conan-bootstrap \
             --platform ${docker_platform} \
-            --tag nexus.homelab/conan-bootstrap-almalinux:latest \
+            --tag "nexus.homelab/conan-bootstrap-almalinux:${machine_arch}-latest" \
             src/almalinux \
 && docker push \
             --platform ${docker_platform} \
-            nexus.homelab/conan-bootstrap-almalinux:latest \
+            "nexus.homelab/conan-bootstrap-almalinux:${machine_arch}-latest" \
 && docker build \
             --target conan-build \
             --build-arg PKG_PREFIX="${PKG_PREFIX}" \
             --build-arg TOOLCHAIN_PREFIX="${TOOLCHAIN_PREFIX}" \
             --platform ${docker_platform} \
-            --tag nexus.homelab/conan-build-almalinux:latest \
+            --tag "nexus.homelab/conan-build-almalinux:${machine_arch}-latest" \
             src/almalinux \
 && docker push \
             --platform ${docker_platform} \
-            nexus.homelab/conan-build-almalinux:latest \
+            "nexus.homelab/conan-build-almalinux:${machine_arch}-latest" \
 && docker build \
             --target conan-docker-build \
             --build-arg PKG_PREFIX="${PKG_PREFIX}" \
             --build-arg TOOLCHAIN_PREFIX="${TOOLCHAIN_PREFIX}" \
             --platform ${docker_platform} \
-            --tag nexus.homelab/conan-docker-build-almalinux:latest \
+            --tag "nexus.homelab/conan-docker-build-almalinux:${machine_arch}-latest" \
             src/almalinux \
 && docker push \
             --platform ${docker_platform} \
-            nexus.homelab/conan-docker-build-almalinux:latest
+            "nexus.homelab/conan-docker-build-almalinux:${machine_arch}-latest" \
 
 exit
 
@@ -68,33 +69,35 @@ cp src/requirements.txt src/ubuntu
 mkdir -p src/ubuntu/config/etc/pki/ca-trust/source/anchors
 cp src/DaverSomethingSomethingRootCA.crt src/ubuntu/config/etc/pki/ca-trust/source/anchors
 
+#            --no-cache \
 docker build \
-            --no-cache \
             --file src/Dockerfile-ubuntu \
             --target conan-base \
             --platform ${docker_platform} \
-            --tag nexus.homelab/conan-base-ubuntu:latest \
+            --tag "nexus.homelab/conan-base-ubuntu:${machine_arch}-latest" \
             src \
 && docker push \
             --platform ${docker_platform} \
-            nexus.homelab/conan-base-ubuntu:latest \
+            "nexus.homelab/conan-base-ubuntu:${machine_arch}-latest" \
 && docker build \
             --file src/Dockerfile-ubuntu \
             --target conan-bootstrap \
             --platform ${docker_platform} \
-            --tag nexus.homelab/conan-bootstrap-ubuntu:latest \
+            --tag "nexus.homelab/conan-bootstrap-ubuntu:${machine_arch}-latest" \
             src \
 && docker push \
             --platform ${docker_platform} \
-            nexus.homelab/conan-bootstrap-ubuntu:latest \
-&& docker build \
-            --file src/Dockerfile-ubuntu \
-            --target conan-build \
-            --build-arg PKG_PREFIX="${PKG_PREFIX}" \
-            --build-arg TOOLCHAIN_PREFIX="${TOOLCHAIN_PREFIX}" \
-            --platform ${docker_platform} \
-            --tag nexus.homelab/conan-build-ubuntu:latest \
-            src \
-&& docker push \
-            --platform ${docker_platform} \
-            nexus.homelab/conan-build-ubuntu:latest
+            "nexus.homelab/conan-bootstrap-ubuntu:${machine_arch}-latest" \
+
+#&& docker build \
+#            --file src/Dockerfile-ubuntu \
+#            --target conan-build \
+#            --build-arg PKG_PREFIX="${PKG_PREFIX}" \
+#            --build-arg TOOLCHAIN_PREFIX="${TOOLCHAIN_PREFIX}" \
+#            --platform ${docker_platform} \
+#            --tag "nexus.homelab/conan-build-ubuntu:${machine_arch}-latest" \
+#            src \
+#&& docker push \
+#            --platform ${docker_platform} \
+#            "nexus.homelab/conan-build-ubuntu:${machine_arch}-latest" \
+
